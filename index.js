2182d5bcb4287be91bd3769cc14eee033b9cc709
Valentin CHEVREAU <vchevreau37@gmail.com>
2019-09-24 19:56:37 +0200

    update gitignore items

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const puppeteer = require('puppeteer');
- 
+// const dotenv = require('dotenv');
+
+//console.log("MY USERNAME", VIADEO_USERNAME);
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   try {
   
-  await page.goto('https://fr.viadeo.com/fr/signin');
+  await page.goto('https://secure.viadeo.com/fr/signin');
  //await page.goto('https://example.com/');
  //await page.focus('#signin > div > main > form > div:nth-child(2) > input[type=text]')
 
+// Identifants de connexion à Viadeo
 await page.click('#signin > div > main > form > div:nth-child(2) > input[type=text]');
 await page.keyboard.type('');
 
@@ -18,10 +22,16 @@ await page.keyboard.type('');
 await page.click('#signin > div > main > form > button');
 
 await page.waitForNavigation();
-await page.goto('https://www.viadeo.com/fr/search/#/?page=1&q=arnaud');
 
-const hrefs = await page.$$eval('a', as => as.map(a => a.href));
-console.log(hrefs)
+// Terme de la recherche
+let value = "arnaud";
+// Requête de la recherche
+await page.goto("https://www.viadeo.com/fr/search/#/?q="+ value, { timeout: 0, waitUntil: "networkidle0" });
+
+// Récupération du lien des comptes
+const hrefs = await page.$$eval('div > div > h2 > a', as => as.map(a => a.href));
+console.log('Liens de profils pour: '+ value + "\n", hrefs);
+
   await browser.close();
 }
 catch(error) {
