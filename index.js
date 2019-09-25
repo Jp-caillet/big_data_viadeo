c76d080b72a641a36b5a0a6d91cec86238127e61
caillet <jp78920@hotmail.com>
2019-09-25 12:15:33 +0200

    add(search by name)

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 const puppeteer = require('puppeteer');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
-//console.log("MY USERNAME", VIADEO_USERNAME);
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  try {
-  
-  await page.goto('https://secure.viadeo.com/fr/signin');
- //await page.goto('https://example.com/');
- //await page.focus('#signin > div > main > form > div:nth-child(2) > input[type=text]')
+(async() => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    dotenv.config();
 
-// Identifants de connexion à Viadeo
-await page.click('#signin > div > main > form > div:nth-child(2) > input[type=text]');
-await page.keyboard.type('');
 
-await page.click('#signin > div > main > form > div:nth-child(3) > input[type=password]');
-await page.keyboard.type('');
+    try {
+        console.log('try to connect signin')
+        await page.goto('https://secure.viadeo.com/fr/signin');
+        console.log('connected')
 
-await page.click('#signin > div > main > form > button');
+        // Identifants de connexion à Viadeo
 
-await page.waitForNavigation();
+        await page.click('#signin > div > main > form > div:nth-child(2) > input[type=text]');
+        await page.keyboard.type(process.env.VIADEO_USER);
+        console.log('cliqued user')
 
-// Terme de la recherche
-let value = "arnaud";
-// Requête de la recherche
-await page.goto("https://www.viadeo.com/fr/search/#/?q="+ value, { timeout: 0, waitUntil: "networkidle0" });
 
-// Récupération du lien des comptes
-const hrefs = await page.$$eval('div > div > h2 > a', as => as.map(a => a.href));
-console.log('Liens de profils pour: '+ value + "\n", hrefs);
+        await page.click('#signin > div > main > form > div:nth-child(3) > input[type=password]');
+        await page.keyboard.type(process.env.VIADEO_PWD);
+        console.log('cliqued password')
 
-  await browser.close();
-}
-catch(error) {
-  console.error(error);
-  // expected output: ReferenceError: nonExistentFunction is not defined
-  // Note - error messages will vary depending on browser
-}
+        await page.click('#signin > div > main > form > button');
+        console.log('wait for redirection')
+        const navigationPromise = page.waitForNavigation();
+
+        try {
+            await navigationPromise
+        } catch (e) {
+            console.log('ueue ca beug')
+        }
+        console.log(hrefs.length)
+        while (hrefs.length == 0) {
+            // Terme de la recherche
+            let value = "arnaud";
+            // Requête de la recherche
+            console.log('try to connect search')
+
+            await page.goto("https://www.viadeo.com/fr/search/#/?q=arnaud");
+            console.log('connected to search')
+
+            // Récupération du lien des comptes
+            const hrefs = await page.$$eval('h2 > a', as => as.map(a => a.href));
+            console.log('Liens de profils pour: ' + value + "\n", hrefs);
+        }
+        await browser.close();
+    } catch (error) {
+        console.error(error);
+        // expected output: ReferenceError: nonExistentFunction is not defined
+        // Note - error messages will vary depending on browser
+    }
 })();
