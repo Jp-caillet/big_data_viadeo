72c62a8176184a33792578c0e0ab62301da97318
caillet <jp78920@hotmail.com>
2019-10-02 15:57:50 +0200

    add crawl in function on name from db

--- index.js
+++ index.js
@@ -1,52 +1,288 @@
 const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
-
+const axios = require('axios');
 
 (async() => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     dotenv.config();
+    let hrefs = [];
 
-
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     try {
-        console.log('try to connect signin')
+        const listName = await axios.get('http://localhost:4000/name/shows')
+        let CurrentName = await axios.get('http://localhost:4000/name/showOne')
+        console.log(CurrentName.data.prenom)
+
+
         await page.goto('https://secure.viadeo.com/fr/signin');
-        console.log('connected')
+
+
+        //await page.goto('https://example.com/');
+        //await page.focus('#signin > div > main > form > div:nth-child(2) > input[type=text]')
 
         // Identifants de connexion à Viadeo
-
+        const navigationPromise = page.waitForNavigation();
         await page.click('#signin > div > main > form > div:nth-child(2) > input[type=text]');
+
         await page.keyboard.type(process.env.VIADEO_USER);
-        console.log('cliqued user')
+
 
 
         await page.click('#signin > div > main > form > div:nth-child(3) > input[type=password]');
         await page.keyboard.type(process.env.VIADEO_PWD);
-        console.log('cliqued password')
+
 
         await page.click('#signin > div > main > form > button');
         console.log('wait for redirection')
-        const navigationPromise = page.waitForNavigation();
-
         try {
             await navigationPromise
         } catch (e) {
             console.log('ueue ca beug')
         }
-        console.log(hrefs.length)
-        while (hrefs.length == 0) {
-            // Terme de la recherche
-            let value = "arnaud";
-            // Requête de la recherche
-            console.log('try to connect search')
+        await sleep(15000);
+        let nb_try = 0;
+        while(true) {
+            CurrentName = await axios.get('http://localhost:4000/name/showOne')
+        axios.post('http://localhost:4000/name/delete', {
+                            name: CurrentName.data.prenom
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
 
-            await page.goto("https://www.viadeo.com/fr/search/#/?q=arnaud");
-            console.log('connected to search')
+            nb_try = 0
+            hrefs = []
 
-            // Récupération du lien des comptes
-            const hrefs = await page.$$eval('h2 > a', as => as.map(a => a.href));
-            console.log('Liens de profils pour: ' + value + "\n", hrefs);
+            while (hrefs.length == 0 && nb_try < 3) {
+                await sleep(2000);
+                // Requête de la recherche
+
+                await page.goto(`https://www.viadeo.com/fr/search/#/?page=1&q=${CurrentName.data.prenom}`);
+
+
+                // Récupération du lien des comptes
+                hrefs = await page.$$eval('h2 > a', as => as.map(a => a.href));
+                console.log('nb_try page 1 :' + nb_try)
+                if (hrefs.length != 0) {
+                    console.log(`page 1: ${CurrentName.data.prenom}`)
+
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[0]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[1]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[2]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[3]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[4]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[5]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[6]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[7]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[8]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[9]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                }
+                nb_try++;
+            }
+
+            hrefs = []
+            nb_try = 0
+            while (hrefs.length == 0 && nb_try < 3) {
+                // Terme de la recherche
+                let value = "arnaud";
+                // Requête de la recherche
+
+                await sleep(2000);
+                await page.goto(`https://www.viadeo.com/fr/search/#/?page=2&q=${CurrentName.data.prenom}`);
+
+
+
+                // Récupération du lien des comptes
+                hrefs = await page.$$eval('h2 > a', as => as.map(a => a.href));
+                console.log('nb_try page 2 :' + nb_try)
+
+
+                if (hrefs.length != 0) {
+                    console.log(`page 2: ${CurrentName.data.prenom}`)
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[0]
+                        })
+                        .then(function(response) {
+                            //console.log(response);
+                        })
+                        .catch(function(error) {
+                            //console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[1]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[2]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[3]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[4]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[5]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[6]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[7]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[8]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                    axios.post('http://localhost:4000/url/create', {
+                            url: hrefs[9]
+                        })
+                        .then(function(response) {
+                            // console.log(response);
+                        })
+                        .catch(function(error) {
+                            // console.log("error");
+                        });
+                }
+                nb_try++;
+
+            }
+
         }
+
+
         await browser.close();
     } catch (error) {
         console.error(error);
